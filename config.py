8435e4cb082c7c644e1c4f7bf45c9954ffcb86f8
Hasan Razin <147445153+hasanrazin@users.noreply.github.com>
2024-02-23 01:29:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,6 +4,6 @@
 # TOKEN = '4213543775:AsfjgfgdsdhfsdjhgjhfgsjgY'
 
 
-API_ID = ''
-API_HASH = ''
-TOKEN = ''
+API_ID = '26945639'
+API_HASH = '5d4215db2e3e6e86b62ac187c98dae32'
+TOKEN = '6949725818:AAEqfqdwsDoV4ZkM7T82EidyRdMOfTFykY4'
